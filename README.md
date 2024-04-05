a447d33258bce10eee035bfa026fdfe558d32315
Emile Jansen <emilemjansen@gmail.com>
2024-04-05 21:08:59 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mailerlite hero section - Test assignment
+This a test assignment: redsign the MailerLite Hero section.
+# mailerlite
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./hero/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
